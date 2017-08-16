2180cc0afc7276e690d52a0c27c96ff1ae2f0ebd
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-08-16 21:42:44 +0200

    example of how to use the package provided

---
+++ inst/examples/app.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(shiny.i18n)
+
+# file with translations
+n18i <- i18n("translation.json")
+
+# change this to en
+n18i$set_translation_language("pl")
+
+ui <- shinyUI(fluidPage(
+  titlePanel(n18i$t("Hello Shiny!")),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  n18i$t("Number of bins:"),
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    mainPanel(
+      plotOutput("distPlot"),
+      p(n18i$t("This is description of the plot."))
+    )
+  )
+))
+
+server <- shinyServer(function(input, output) {
+
+  output$distPlot <- renderPlot({
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins,
+         col = "darkgray", border = "white",
+         main = n18i$t("Histogram of x"), ylab = n18i$t("Frequency"))
+  })
+})
+
+shinyApp(ui = ui, server = server)
