bf903fdf6570520be52f2e93f247509583f2d2d4
Eduardodudu <digdins@gmail.com>
2023-07-05 12:50:29 -0300

    add translate_with_google_cloud unit test

---
+++ tests/testthat/test_automatic.R
@@ -0,0 +1,20 @@
+context("automatic")
+
+test_that("translate_with_google_cloud", {
+  it("Should check if API is set", {
+    withr::with_envvar(
+      new = c(
+        GL_AUTH = "None"
+      ),{
+        # Arrange
+        txt <- "Hello, how are you?"
+        target_lang <- "fr"
+        # Act
+        result <- evaluate_promise(
+          translate_with_google_cloud(txt, target_lang)
+        )
+        # Assert
+        expect_equal(length(result$messages), 7)
+    })
+  })
+})
