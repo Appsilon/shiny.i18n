44827c9733f51f83b4dc7cdf6391ef31140af172
Eduardodudu <digdins@gmail.com>
2023-07-06 15:13:12 -0300

    Add unit test for .onLoad

--- tests/testthat/test_zzz.R
+++ tests/testthat/test_zzz.R
@@ -9,3 +9,16 @@ test_that("test get_i18n_config", {
   expect_type(get_i18n_config("cultural_bignumer_mark"), "character")
   expect_error(get_i18n_config("cultural_bignumer_markx"))
 })
+
+test_that("Test .onLoad", {
+  # Assert
+  expect_equal(
+    class(.i18_config),
+    c("list")
+  )
+  expect_equal(length(.i18_config), 3)
+  expect_equal(
+    names(.i18_config),
+    c("cultural_date_format", "cultural_bignumer_mark", "cultural_punctuation_mark")
+  )
+})
